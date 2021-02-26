be7cff933f6cfc9f62deca5ae5f647839bbd0fdc
Nunes <GilsonNunes_dev@outlook.com>
2021-02-26 00:11:53 -0300

    added context to the Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/Profile.module.css'
 
 export default function Profile() {
+  const {level} = useContext(ChallengeContext)
+
   return (
     <div className={styles.profileContainer}>
       <img
@@ -11,7 +15,7 @@ export default function Profile() {
         <strong>Username</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
